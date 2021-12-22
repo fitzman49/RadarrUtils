be934f6102dbb0cdd9471153af8b1f4faa0a6576
fitzman49 <5747425+fitzman49@users.noreply.github.com>
2021-12-22 12:34:54 -0500

    Create AC3Encode.sh

---
+++ AC3Encode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIE_PATH=${radarr_moviefile_path}
+if [ "${radarr_eventtype}" = "Test" ]; then
+  MOVIE_PATH="/video/movies/Zombieland Double Tap (2019)/Zombieland Double Tap >
+  echo "Hit Test Mode"
+fi
+
+TEMP_FILE="$MOVIE_PATH.ac3.mp4"
+ffmpeg -i "${MOVIE_PATH}" -map 0 -vcodec copy -scodec copy -acodec ac3 -b:a 640>
+mv "$TEMP_FILE" "$MOVIE_PATH"
+
+echo "Converted ${MOVIE_PATH} to AC3"
